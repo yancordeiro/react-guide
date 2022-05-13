af1bc223cb24b389d498b0b3f2cd11ee9ddd911c
yancordeiro <47543105+yancordeiro@users.noreply.github.com>
2022-05-12 22:44:58 -0300

    React guide academind

---
+++ src/bkp/components/Expenses/ExpenseItem.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated")
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">R${props.amount}</div>
+      </div>
+
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+ExpenseItem.propTypes = {};
+
+export default ExpenseItem;
+
